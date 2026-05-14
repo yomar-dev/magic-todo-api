716d8b2efbfe80b5c10e4613a72792ead3d816bb
Yomar Miranda <mirandayomar@gmail.com>
2026-05-14 15:33:20 -0500

    fix: handle undefined case for connection string in prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,7 +3,11 @@ import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../../generated/prisma/client.js';
 import { config } from '../config/index.js';
 
-const connectionString = config.database.url!;
+const connectionString = config.database.url;
+
+if (!connectionString) {
+  throw new Error('Database URL is not configured');
+}
 
 const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
